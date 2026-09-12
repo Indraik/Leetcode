d7902be712464944f11e0d0a484ef45b6198b7a5
Indra Kumar V <134196372+Indraik@users.noreply.github.com>
2026-09-12 21:59:47 +0530

    Time: 350 ms (20.73%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,4 @@
 select max(salary) as SecondHighestSalary from (
-    select salary,
-    DENSE_RANK() over (order by salary desc) as rnk
-    from Employee
-) as rankedsalary
-where rnk = 2;
+select DISTInct salary from Employee
+order by salary desc limit 1 offset 1
+) as q
